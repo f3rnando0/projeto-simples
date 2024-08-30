cf98e7643d0a0741ca096043195e1487b24d5e3f
foolish8932 <contasuch123@gmail.com>
2024-08-30 10:58:26 -0300

    feat: added data table with mock data for now

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,7 +1,79 @@
+"use client";
+
+import type { ColumnDef } from "@tanstack/react-table";
+import { DataTable } from "./components/Table";
+import { XIcon } from "lucide-react";
+
+type Product = {
+	id: string;
+	amount: number;
+	valor_unitario: number;
+	valor: number;
+	peso: number;
+	volume: number;
+	prazo_minimo?: string;
+	prazo_maximo?: string;
+	descricao?: string;
+};
+
+export const produtos: Product[] = [
+	{
+		id: "728ed52f",
+		amount: 100,
+		valor_unitario: 19.99,
+		valor: 1999,
+		peso: 1.5,
+		volume: 0.5,
+		prazo_minimo: "15 dias",
+		prazo_maximo: "30 dias",
+		descricao: "Papel para escrita",
+	},
+];
+
+export const columns: ColumnDef<Product>[] = [
+	{
+		accessorKey: "id",
+		header: "Ações",
+		cell: ({ row }) => <XIcon className="cursor-pointer" />,
+	},
+	{
+		accessorKey: "amount",
+		header: "Quantidade",
+	},
+	{
+		accessorKey: "valor_unitario",
+		header: "Valor unitário",
+	},
+	{
+		accessorKey: "valor",
+		header: "Valor",
+	},
+	{
+		accessorKey: "peso",
+		header: "Peso",
+	},
+	{
+		accessorKey: "volume",
+		header: "Volume",
+	},
+	{
+		accessorKey: "prazo_minimo",
+		header: "Prazo mínimo",
+	},
+	{
+		accessorKey: "prazo_maximo",
+		header: "Prazo máximo",
+	},
+	{
+		accessorKey: "descricao",
+		header: "Descrição",
+	},
+];
+
 export default function Form() {
 	return (
 		<div className="p-4">
-			<div>testnado</div>
+			<DataTable columns={columns} data={produtos} />
 		</div>
 	);
 }
