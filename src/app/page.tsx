4f87fc6e9010383289ea9e90418a6d71f9bad16b
foolish8932 <contasuch123@gmail.com>
2024-08-30 10:41:13 -0300

    feat: project inicialization

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Form from "@/components/Form";
+import Header from "@/components/Header";
+import Link from "next/link";
+
+export default function HomePage() {
+	return (
+		<div className="w-full h-screen">
+			<Header />
+			<Form />
+		</div>
+	);
+}
---
+++ src/components/Form/index.tsx
@@ -0,0 +1,7 @@
+export default function Form() {
+	return (
+		<div className="p-4">
+			<div>testnado</div>
+		</div>
+	);
+}
